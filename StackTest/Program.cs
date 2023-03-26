053cc4551e93d6139011cdb9448199fead75ca5d
yulmumu <87139443+yulmumu@users.noreply.github.com>
2023-03-27 02:52:44 +0900

    Add files via upload

---
+++ StackTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        LinkedList<String> countries = new LinkedList<String>();
+        countries.AddLast("India");
+        countries.AddLast("Mexico");
+        countries.AddLast("Japan");
+        countries.AddLast("Nepal");
+        countries.AddLast("Korea");
+
+        LinkedListNode<String> node = countries.Find("Nepal");
+        countries.AddBefore(node, "Australia");
+        countries.AddAfter(node, "Canada");
+ 
+        foreach (var country in countries)
+        {
+            Console.WriteLine(country);
+        }
+        
+        Console.WriteLine("----------");
+        countries.Remove(countries.First);
+        foreach (var country in countries)
+        {
+            Console.WriteLine(country);
+        }
+        
+        Console.WriteLine("----------");
+        countries.Remove("Australia");
+        foreach (var country in countries)
+        {
+            Console.WriteLine(country);
+        }
+    }
+}
